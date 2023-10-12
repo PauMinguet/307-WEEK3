5801008f02a9104781d993f98d8160c00da7b651
PauMinguet <pablitumm@gmail.com>
2023-10-11 22:56:12 -0700

    asdf

--- jest-unit-testing/module.js
+++ jest-unit-testing/module.js
@@ -1,16 +1,37 @@
-function sum(a, b) {
-    return a + b;
-  }
-function div (a, b){
-    return a / b;
-  }
-  
-function containsNumbers(text){
-    for (let i = 0; i < text.length; i++) {
-        if (!isNaN(text.charAt(i)))
-        return true;
-    }
-    return false;
-  }
 
-export default { sum, div, containsNumbers };
+
+// CALCULATOR
+
+function sum (a, b){
+  return a + b;
+}
+function sub (a, b){
+  return a - b;
+}
+function mult (a, b){
+  return a * b;
+}
+function div (a, b){
+  return a / b;
+}
+
+function two_digit_calc (str){
+  if (str.length != 5){
+    return NaN
+  }
+  let num1 = Math.floor(str[0] + str[1])
+  let num2 = Math.floor(str[3] + str[4])
+
+  if (str[2] === "+"){
+      return sum(num1, num2)
+  } else if (str[2] === "-"){
+      return sub(num1, num2)
+  } else if (str[2] === "*"){
+      return mult(num1, num2)
+  } else if (str[2] === "/"){
+      return div(num1, num2)
+  } else { return NaN}
+}
+
+
+export default { two_digit_calc };
--- jest-unit-testing/module.test.js
+++ jest-unit-testing/module.test.js
@@ -2,24 +2,42 @@ import mut from './module.js'; // MUT = Module Under Test
 
 test('Testing sum -- success', () => {
   const expected = 30;
-  const got = mut.sum(12, 18);
+  const got = mut.two_digit_calc("12+18");
+  expect(got).toBe(expected);
+});
+
+test('Testing sub -- success', () => {
+  const expected = 30;
+  const got = mut.two_digit_calc("43-13");
+  expect(got).toBe(expected);
+});
+
+test('Testing mult -- success', () => {
+  const expected = 30;
+  const got = mut.two_digit_calc("03*10");
   expect(got).toBe(expected);
 });
 
 test('Testing div -- success', () => {
-    const expected = 5;
-    const got = mut.div(30, 6);
-    expect(got).toBe(expected);
-  });
+  const expected = 30;
+  const got = mut.two_digit_calc("90/03");
+  expect(got).toBe(expected);
+});
 
-test('Testing containsNumbers -- success', () => {
-    const expected = false;
-    const got = mut.containsNumbers("ajslkfjaslñdfj");
-    expect(got).toBe(expected);
-  });
+test('Testing wrong operator -- success', () => {
+  const expected = NaN;
+  const got = mut.two_digit_calc("12{08");
+  expect(got).toBe(expected);
+});
 
-test('Testing containsNumbers -- success', () => {
-    const expected = false;
-    const got = mut.containsNumbers(" ");
-    expect(got).toBe(expected);
-  });
+test('Testing wrong string length -- success', () => {
+  const expected = NaN;
+  const got = mut.two_digit_calc("12{081");
+  expect(got).toBe(expected);
+});
+
+test('Testing non number input -- success', () => {
+  const expected = NaN;
+  const got = mut.two_digit_calc("a1+23");
+  expect(got).toBe(expected);
+});
