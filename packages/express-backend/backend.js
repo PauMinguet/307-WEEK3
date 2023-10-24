efdcc144b11398f78150e69a38e3638a5ecf2a0e
PauMinguet <pablitumm@gmail.com>
2023-10-23 22:08:01 -0700

    debug as much as i could manage

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -12,9 +12,9 @@ app.use(express.json());
 
 app.delete('/users/:id', (req, res) => {
     const id = req.params['id'];
-    let result = userModel.findByIdAndDelete(id).then(() => res.status(201).json({ message : "User deleted successfully", }).catch((error) => { message : "Eror when deleting user"}));
-    res.status(404).send("User deleted successfully");
-    
+    userModel.findByIdAndDelete(id)
+    .then(() => res.status(201).json({ message : "User deleted successfully", }))
+    .catch((error) => { message : "Eror when deleting user"});    
 });
 
 
@@ -22,11 +22,9 @@ app.delete('/users/:id', (req, res) => {
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
 
-    userToAdd._id = Math.floor(Math.random()*1000)
-
-    userModel.addUser(userToAdd).then(() => res.status(201).json({ message : "User created successfully", 
-        user: userToAdd
-    }).catch((error) => { message : "Eror when creating user"}))
+    userModel.addUser(userToAdd).then((useradded) => res.status(201).json({ message : "User created successfully", 
+        user: useradded
+    })).catch((error) => { message : "Eror when creating user"})
 
 });
 
@@ -48,10 +46,8 @@ app.get('/users', (req, res) => {
     const job = req.params['job']; //or req.params.id
     console.log(job)
     
-    result = userModel.getUsers(name, job)
-    .then((response) => 
-    res.status(200)
-    .send(response))
+    userModel.getUsers(name, job)
+    .then((response) => res.status(200).send(response))
     .catch((error) => { message : "Error when fetching users"})
 
 });
